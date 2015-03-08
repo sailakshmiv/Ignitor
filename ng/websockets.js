d4192ceb6ec05023945ada6ad6881480a3e36650
Michael Chavez <bigbassroller@gmail.com>
2015-03-07 22:19:03 -0800

    added reconnection

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,16 +1,18 @@
 angular.module('app')
-.run(function ($rootScope) {
-	var url = 'ws://localhost:3000'
-	var connection = new WebSocket(url)
+.run(function ($rootScope, $timeout) {
+	(function connect() {
+		var url = 'ws://localhost:3000'
+		var connection = new WebSocket(url)
 
-	connection.onopen = function () {
-		console.log('WebSocket connected')
-	}
-	connection.onmessage = function (e) {
-		console.log(e)
-		var payload = JSON.parse(e.data)
-		$rootScope.$broadcast('ws:' + payload.topic, payload.data)
-	}
+		connection.onopen = function () {
+			console.log('WebSocket connected')
+		}
+		connection.onmessage = function (e) {
+			console.log(e)
+			var payload = JSON.parse(e.data)
+			$rootScope.$broadcast('ws:' + payload.topic, payload.data)
+		}
+	})()
 })
 
 
