83e66c13d69d27d969e9d6affd54f05f941537fc
Michael Chavez <bigbassroller@gmail.com>
2015-03-08 04:17:19 -0700

    created pubsub branch. Pubsub is currently not working

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,6 +1,7 @@
 var Post = require('../../models/post')
 var router = require('express').Router()
 var websockets = require('../../websockets')
+var pubsub = require('../../pubsub')
 
 router.get('/', function (req, res, next) {
   Post.find() 
@@ -16,9 +17,13 @@ router.post('/', function (req, res, next) {
   post.username = req.auth.username
   post.save(function (err, post) {
     if (err) { return next(err) }
-    websockets.broadcast('new_post', post)
+    pubsub.publish('new_post', post)
     res.status(201).json(post)
   })
+})
+
+pubsub.subscribe('new_post', function (post) {
+  websockets.broadcast('new_post', post)
 })
 
 module.exports = router
