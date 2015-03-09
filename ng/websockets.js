cdec56217d275f5a9b53acb99c2da2a522193e2d
Michael Chavez <bigbassroller@gmail.com>
2015-03-09 14:03:05 -0700

    Publishing events from the client

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,18 +1,28 @@
 angular.module('app')
-.run(function ($rootScope, $timeout) {
-	(function connect() {
+.service('WebSocketSvc', function ($rootScope, $timeout) {
+	var connection
+	this.connect = function () {
 		var url = 'ws://localhost:3000'
-		var connection = new WebSocket(url)
+		connection = new WebSocket(url)
 
 		connection.onopen = function () {
 			console.log('WebSocket connected')
 		}
+
 		connection.onmessage = function (e) {
 			console.log(e)
+			console.log('On message event')
 			var payload = JSON.parse(e.data)
 			$rootScope.$broadcast('ws:' + payload.topic, payload.data)
 		}
-	})()
+
+	}
+	this.send = function (topic, data) {
+		var json = JSON.stringify({topic: topic, data: data})
+		connection.send(json)
+	}
+}).run(function (WebSocketSvc) {
+	WebSocketSvc.connect()
 })
 
 
