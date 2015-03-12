ac3b3a6f1ff78ea0249111fcd6268cc893467e03
Michael Chavez <bigbassroller@gmail.com>
2015-03-12 01:35:00 -0700

    Http testing with Karma

--- test/ng/post.svc.spec.js
+++ test/ng/post.svc.spec.js
@@ -1,14 +1,29 @@
 describe('posts.svc', function () {
 	beforeEach(module('app'))
-	var PostsSvc
+	var PostsSvc, $httpBackend
 
-	beforeEach(inject(function (_PostsSvc_) {
+	beforeEach(inject(function (_PostsSvc_, _$httpBackend_) {
 		PostsSvc = _PostsSvc_
+		$httpBackend = _$httpBackend_
 	}))
 
+	afterEach(function () {
+		$httpBackend.flush()
+	})
+
 	describe('#fetch', function () {
-		it('exists', function () {
-			expect(PostsSvc.fetch).to.exist
+		beforeEach(function () {
+			$httpBackend.expect('GET', '/api/posts')
+			.respond([
+				{username: 'bigbassrollertest', body: 'first post'},
+				{username: 'bigbassrollertest', body: 'second post'}
+				])
+		})
+
+		it('gets 2 posts', function () {
+			PostsSvc.fetch().success(function (posts) {
+				expect(posts).to.have.length(2)
+			})
 		})
 	})
 })
