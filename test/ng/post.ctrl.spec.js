a3153b131cce067b71bc323de5371321c68717ed
Michael Chavez <bigbassroller@gmail.com>
2015-03-13 01:57:23 -0700

    Karma controller test completed

--- test/ng/post.ctrl.spec.js
+++ test/ng/post.ctrl.spec.js
@@ -2,14 +2,28 @@ describe('posts.ctrl', function () {
 	beforeEach(module('app'))
 	var $scope
 
+	var mockPostSvc = {}
+	beforeEach(inject(function ($q) {
+		mockPostSvc.fetch = function () {
+			var deferred = $q.defer()
+			deferred.resolve([
+				{ username: 'bigbassrollertest', body: 'first post'},
+				{ username: 'bigbassrollertest', body: 'second post'}
+			])
+			return deferred.promise
+		}
+	}))
+
 	beforeEach(inject(function ($rootScope, $controller) {
 		$scope = $rootScope.$new()
 		$controller('postsCtrl', {
-			$scope: $scope
+			$scope: $scope,
+			PostsSvc: mockPostSvc
 		})
 	}))
 
 	it('loads posts from the service', function () {
+		$scope.$digest()
 		expect($scope.posts).to.have.length(2)
 	})
 })
