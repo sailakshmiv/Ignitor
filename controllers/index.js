3a1c34cc1209db266749133eb74f819ade79c5c7
Michael Chavez <bigbassroller@gmail.com>
2015-03-18 10:21:48 -0700

    changed sessions mounting

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
-var router = require('express').Router()
 var bodyParser = require('body-parser')
-
-router.use(bodyParser.json())
+var router = require('express').Router()
 
 router.use(require('../auth'))
-router.use('/api/sessions',  require('./api/sessions'))
-router.use('/api/users',  require('./api/users'))
-router.use('/api/posts',  require('./api/posts'))
-router.use( require('./static'))
+router.use(bodyParser.json())
+
+router.use('/api', require('./api'))
+router.use('/', require('./static'))
 
 module.exports = router
